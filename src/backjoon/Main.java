543634a9f2326dbbad9abd9785fef15e078239ab
soo <ksbsj92@naver.com>
2024-06-22 15:27:08 +0900

    # 백준 10430

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -6,9 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
 
-        System.out.println(s + "??!");
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+       int C = sc.nextInt();
+
+        System.out.println( (A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println( (A*B)%C);
+        System.out.println(  ((A%C) * (B%C))%C);
 
     }
 }
