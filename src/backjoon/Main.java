ad9f43c6ee076a89fe3168d4edcdf4081bee12bd
soo <ksbsj92@naver.com>
2024-06-22 15:47:04 +0900

    # 백준 9498

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
+       Scanner sc = new Scanner(System.in);
+
+       int n = sc.nextInt();
+
+       if(n >= 90 & n <= 100) {
+           System.out.println("A");
+       } else if(n >= 80 & n < 90) {
+           System.out.println("B");
+       } else if(n >= 70 & n < 80) {
+           System.out.println("C");
+       } else if(n >= 60 & n < 70) {
+           System.out.println("D");
+       } else {
+           System.out.println("F");
+       }
 
     }
 }
