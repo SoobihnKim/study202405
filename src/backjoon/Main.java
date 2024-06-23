6a3dd60516a869d7609b115439047f63cc925013
soo <ksbsj92@naver.com>
2024-06-23 15:45:43 +0900

    # 백준 14681

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -10,12 +10,14 @@ public class Main {
        int A = sc.nextInt();
        int B = sc.nextInt();
 
-       if(A > B) {
-           System.out.println(">");
-       } else if(A < B) {
-            System.out.println("<");
-        } else {
-           System.out.println("==");
+       if(A > 0 & B > 0) {
+           System.out.println(1);
+       } else if (A < 0 & B > 0) {
+           System.out.println(2);
+       } else if (A < 0 & B < 0) {
+           System.out.println(3);
+       } else if (A > 0 & B < 0) {
+           System.out.println(4);
        }
     }
 }
