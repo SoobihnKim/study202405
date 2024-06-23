247434b14dcfd89e822e1c82bd849b247cadf9a4
soo <ksbsj92@naver.com>
2024-06-23 15:42:15 +0900

    # 백준 1330

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -7,13 +7,15 @@ public class Main {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
 
-       int n = sc.nextInt();
+       int A = sc.nextInt();
+       int B = sc.nextInt();
 
-       if(n % 4 == 0 & (n % 100 != 0 || n % 400 == 0)) {
-           System.out.println(1);
-       } else {
-           System.out.println(0);
+       if(A > B) {
+           System.out.println(">");
+       } else if(A < B) {
+            System.out.println("<");
+        } else {
+           System.out.println("==");
        }
-
     }
 }
