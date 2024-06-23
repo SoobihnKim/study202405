9850c2a529b5f216d068d424a74516e9e0607b53
soo <ksbsj92@naver.com>
2024-06-23 16:09:01 +0900

    # 백준 2525

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // 오븐시계
         Scanner sc = new Scanner(System.in);
 
-        int H = sc.nextInt();
-        int M = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
 
-        if (M < 45) {
-            H--;
-            M = 60 - (45 - M);
-            if (H < 0) {
-                H = 23;
-            }
-            System.out.println(H + " " + M);
-        } else {
-            System.out.println(H + " " + (M - 45));
-        }
+        int min = 60 * A + B; // 분
+        min += C;
+
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " + minute);
     }
 }
