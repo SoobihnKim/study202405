d39bd5b12f3c8b465d97dc24be5f78cd53b9f9aa
soo <ksbsj92@naver.com>
2024-08-24 14:37:51 +0900

    # 7번 / 가습기

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -6,25 +6,50 @@ public class Main {
 
     public static int[] main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int start = sc.nextInt();
-        int before = sc.nextInt();
-        int after = sc.nextInt();
+        class Solution {
+            public int func1(int humidity, int val_set) {
+                if (humidity < val_set)
+                    return 3;
+                return 1;
+            }
 
-        int money = start;
-        int month = 1;
-        while (money < 70) {
-            money += before;
-            month++;
+            public int func2(int humidity) {
+                if (humidity >= 50)
+                    return 0;
+                else if (humidity >= 40)
+                    return 1;
+                else if (humidity >= 30)
+                    return 2;
+                else if (humidity >= 20)
+                    return 3;
+                else if (humidity >= 10)
+                    return 4;
+                else
+                    return 5;
+            }
+
+            public int func3(int humidity, int val_set) {
+                if (humidity < val_set)
+                    return 1;
+                return 0;
+            }
+
+            public int solution(String mode_type, int humidity, int val_set) {
+                int answer = 0;
+
+                if (mode_type.equals("auto")) {
+                    answer = func2(humidity);
+                } else if (mode_type.equals("target")) {
+                    answer = func1(humidity, val_set);
+                } else if (mode_type.equals("minimum")) {
+                    answer = func3(humidity, val_set);
+                }
+
+                return answer;
+            }
         }
-        while (money < 100) {
-            money += after;
-            month++;
-        }
-        System.out.println(month);
-        return new int[0];
+        return null;
     }
 }
-
 
 
