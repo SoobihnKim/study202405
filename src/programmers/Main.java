2c9e4088ba6332e07ac6cc6f7335c44973bcfd64
soo <ksbsj92@naver.com>
2024-09-27 14:24:04 +0900

    # 홀수 vs 짝수

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -3,14 +3,19 @@ package programmers;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if(n % 2 == 0) {
-            System.out.println(n + " is even");
-        } else {
-            System.out.println(n + " is odd");
+    public int solution(int[] num_list) {
+        int oddsum = 0;
+        int evensum = 0;
+
+        for(int i = 0; i < num_list.length; i++) {
+            if(i % 2 == 0) {
+                oddsum += num_list[i];
+            } else {
+                evensum += num_list[i];
+            }
         }
+
+        return Math.max(oddsum, evensum);
     }
 }
 
