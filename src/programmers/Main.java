707f31cbf9b04fa0a2a2d25c871a111906aa6af2
soo <ksbsj92@naver.com>
2024-10-03 19:12:52 +0900

    # 배열의 길이에 따라 다른 연산하기

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public int solution(int[] num_list) {
-        int product = 1;  // 곱을 저장할 변수, 1로 초기화
-        int sum = 0;      // 합을 저장할 변수
-
-        // 리스트의 원소들을 순회하면서 곱과 합을 계산
-        for (int num : num_list) {
-            product *= num;  // 원소들의 곱 계산
-            sum += num;      // 원소들의 합 계산
+    public int[] solution(int[] arr, int n) {
+        int[] answer = arr.clone();
+        if(arr.length % 2 == 0) {
+            for (int i = 1; i < arr.length; i+=2) {
+                answer[i] += n;
+            }
+        } else {
+            for (int i = 0; i < arr.length; i+=2) {
+                answer[i] += n;
+            }
         }
-
-        // 곱이 합의 제곱보다 작으면 1, 크거나 같으면 0 반환
-        return product < (sum * sum) ? 1 : 0;
+            return answer;
     }
 }
 
