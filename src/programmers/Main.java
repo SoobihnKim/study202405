3481bcbe1314203f7e701bdc31ec517d6a7bff7b
soo <ksbsj92@naver.com>
2024-10-04 15:22:07 +0900

    # 0 떼기

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -4,18 +4,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public int[] solution(int[] arr, int n) {
-        int[] answer = arr.clone();
-        if(arr.length % 2 == 0) {
-            for (int i = 1; i < arr.length; i+=2) {
-                answer[i] += n;
-            }
-        } else {
-            for (int i = 0; i < arr.length; i+=2) {
-                answer[i] += n;
-            }
-        }
-            return answer;
+    public String solution(String n_str) {
+        String answer = String.valueOf(Integer.parseInt(n_str));
+        return answer;
     }
 }
 
