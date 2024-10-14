e98ced274ce5a6a5b14dafcfd9e2e016a9d7b939
soo <ksbsj92@naver.com>
2024-10-14 20:21:49 +0900

    # 문자열 정수의 합

--- src/programmers/Main.java
+++ src/programmers/Main.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public int solution(String n_str) {
-        int answer = Integer.parseInt(n_str);
-        return answer;
+    public int solution(String num_str) {
+        int sum = 0;
+        for (int i = 0; i < num_str.length(); i++) {
+            sum += Character.getNumericValue(num_str.charAt(i));
+        }
+        return sum;
     }
 }
 
